107d5bd37ed6826e4819d10a3b53b4edc63e80d5
IT24100172 <it24100172@my.sliit.lk>
2024-09-02 21:38:34 +0530

    Add IT24100172Lab6Q2A.java to Q2A

---
+++ Q2/IT24100172Lab6Q2A.java
@@ -0,0 +1,14 @@
+public class IT24100172Lab6Q2A{
+public static void main(String[] args){
+
+int number;
+
+number =1;
+while (number<=10)
+{
+ System.out.print(number+" ");
+ number=number+1;
+}
+
+}
+}
